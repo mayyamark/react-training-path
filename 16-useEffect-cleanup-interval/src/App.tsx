1a75f260db267b5affca77925bef00aae8e3ebac
Mayya Markova <mayya.markova@gmail.com>
2023-09-07 22:34:01 +0300

    Add useEffect cleanup interval demo

---
+++ 16-useEffect-cleanup-interval/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);    
+    }, 1000);
+  
+    return () => clearInterval(interval);
+  }, [count]);
+  
+
+  return (
+    <div>
+      Counter: {count}
+    </div>
+  );
+};
+
+export default App;
