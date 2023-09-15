be9e7b4c7c46eeb90df1eb48ce308ae9d546f1c2
Mayya Markova <mayya.markova@gmail.com>
2023-09-15 17:15:27 +0300

    Add useDeferredValue demo

---
+++ 33-useDeferredValue/src/List.tsx
@@ -0,0 +1,23 @@
+import { useDeferredValue, useMemo } from "react";
+
+const numbers = [...new Array(200000).keys()];
+
+interface ListProps {
+  query: string;
+}
+
+const List: React.FC<ListProps> = ({ query }) => {
+  const deferredQuery = useDeferredValue(query);
+
+  const list = useMemo(() => (
+    numbers
+      .filter((i) => (
+        deferredQuery.length === 0 || i.toString().startsWith(deferredQuery)
+      ))
+      .map(i => <p key={i}>{i}</p>)
+  ), [deferredQuery]);
+  
+  return <div>{list}</div>;
+}
+
+export default List;
