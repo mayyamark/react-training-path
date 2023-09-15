256128e27e8876bab7ecc869e8506873d0b4eb25
Mayya Markova <mayya.markova@gmail.com>
2023-09-15 13:17:14 +0300

    Add useImperativeHandle demo

---
+++ 28-useImperativeHandle/src/TextArea.tsx
@@ -0,0 +1,21 @@
+import { forwardRef, useImperativeHandle, useRef } from "react"
+
+const TextArea = forwardRef((props, ref) => {
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus() {
+        if (textAreaRef.current) {
+          textAreaRef.current.focus();
+          textAreaRef.current.style.backgroundColor = "aliceblue";
+        }
+        console.log("Custom focus called!")
+      }
+    }
+  }, []);
+
+  return <textarea {...props} ref={textAreaRef} />
+});
+
+export default TextArea;
