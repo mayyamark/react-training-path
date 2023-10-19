87c73f12e31dad1e988fdb6e448185dcb80bc013
Mayya Markova <mayya.markova@gmail.com>
2023-10-19 13:14:54 +0300

    Implement /favourites page

--- gifest/src/App/App.tsx
+++ gifest/src/App/App.tsx
@@ -1,15 +1,54 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../Layout/Layout";
-import TrendingPage from "../TrendingPage/TrendingPage";
+import GifsPage from "../GifsPage/GifsPage";
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
+import { API_KEY, GET_ENDPOINT } from "../constants";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [favourites, setFavourites] = useState<string[]>(JSON.parse(localStorage.getItem('favourite-gifs') as string) || []);
+
+  useEffect(() => {
+    localStorage.setItem('favourite-gifs', JSON.stringify(favourites));
+  }, [favourites]);
+
+  const handleFavouriteGif = (id: string) => {
+    setFavourites((prev) => {
+      if (prev.includes(id)) {
+        return prev.filter((i) => i !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
+  }
+
   return (
     <ErrorBoundary>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
-            <Route path='/' index element={<TrendingPage />} />
+            <Route 
+              path='/' 
+              index 
+              element={
+                <GifsPage 
+                  url={`${GET_ENDPOINT}/trending?api_key=${API_KEY}`}
+                  title='Trending'
+                  onDoubleClickGif={handleFavouriteGif}
+                />
+              } 
+            />
+            <Route 
+              path='/favourites' 
+              element={
+                <GifsPage
+                  url={`${GET_ENDPOINT}?ids=${favourites.join(',')}&api_key=${API_KEY}`}
+                  title='Favourites'
+                  onDoubleClickGif={handleFavouriteGif}
+                  infiniteScroll={false}
+                />
+              } 
+            />
           </Route>
         </Routes>
       </BrowserRouter>
